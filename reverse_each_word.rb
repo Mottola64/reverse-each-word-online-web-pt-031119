7190134ca28bb8425449df77055f3a133755b5a4
Michael Mottola <mottola64@gmail.com>
2019-03-22 02:27:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,4 @@
 def reverse_each_word(sentence)
-  
-  #split
-  
-  sentence.split
-  new_sentence = []
-  sentence_array = sentence.split
-  sentence_array.collect do |word|
-       new_sentence << word.reverse
-end
-new_sentence.join(" ")
-end
-
-
-def reverse_each_word
   sentence.split
   sentence_array = sentence.split
   sentence_array.collect do |word|
