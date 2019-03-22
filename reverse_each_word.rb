5c75185fcb288cd930f8b9a1458e844928c535a1
Michael Mottola <mottola64@gmail.com>
2019-03-22 02:24:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,6 @@ def reverse_each_word(sentence)
   sentence_array.collect do |word|
     new_sentence = []
    new_sentence << word.reverse
-
-new_sentence.join(" ")
 end
+new_sentence.join(" ")
 end
