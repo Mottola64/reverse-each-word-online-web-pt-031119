088d08423fc61241e0ace3139bca5b6da9eed769
Michael Mottola <mottola64@gmail.com>
2019-03-22 02:20:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,8 @@ def reverse_each_word(sentence)
   
   sentence_array = sentence.split
   sentence_array.collect do |word|
-    new_sentence = word.reverse
+    new_sentence = []
+    word.reverse
 end
 new_sentence.join(" ")
 end
